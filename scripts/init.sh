f0a11b6e6fe9e25693845cbf878cdde7b8df7bbf
Duncan Horn <dunhor@microsoft.com>
2025-05-07 17:03:51 -0700

    Is it a symlink?

--- scripts/init.sh
+++ scripts/init.sh
@@ -109,7 +109,7 @@ if [ "$vcpkgRoot" == "" ]; then
         # Try and find vcpkg in the PATH
         vcpkgPath=$(/bin/which vcpkg)
         if [ $? == 0 ]; then
-            vcpkgRoot=$(dirname $vcpkgPath)
+            vcpkgRoot=$(realpth $vcpkgPath | dirname)
         fi
     fi
 fi
